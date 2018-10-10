2809c69bbc2c56df8e9a16c7c325575f1953a203
LdyaiSj <1581970748@qq.com>
2018-10-10 12:22:05 +0800

    v-0.71

--- assets/js/index4.js
+++ assets/js/index4.js
@@ -75,18 +75,6 @@ $(document).ready(function () {
      }
     }
 
-//错误时显示
-    function renwuwrong($t) {
-      $t.find('.animation-wrong').velocity('transition.shrinkIn', {
-        duration: 700,
-        complete: function () {
-          const $t = $(this);
-          let sjNewdata = data.daoju[classData][sjCount];
-          $t.velocity('fadeOut');
-          sjInitData(sjNewdata)
-        }
-      })
-    }
 
 //放弃回答
     $('.abandon').on('click', function() {
@@ -100,7 +88,7 @@ $(document).ready(function () {
       if (sjIsFinished == 10) {
         $('.result-title3 span').text(topic)
         $('.result-renwu-box').velocity('fadeIn', {
-          duration: 2000,
+          duration: 3000,
           complete: function () {
             let $t = $(this);
             if (topic >= 4 && topic < 8) {
